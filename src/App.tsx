9148cd84b7215ba09fa177b02a00f1cd97ef1225
BryanGordon <bryanbge92@gmail.com>
2024-06-13 18:54:47 -0500

    Update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
-import { useEffect, useState } from 'react'
 import './App.css'
+import { useEffect, useRef, useState } from 'react'
+import { type User } from './types'
+import { UsersList } from './components/UsersList'
 
 function App () {
-  const [users, setUsers] = useState([])
+  const originalUsers = useRef<User[]>([])
+  const [users, setUsers] = useState<User[]>([])
+  const [showColors, setShowColors] = useState(false)
+  const [countrySort, setCountrySort] = useState(false)
+
+  const toggleColors = () => {
+    setShowColors(!showColors)
+  }
+
+  const toggleSortCountry = () => {
+    setCountrySort(!countrySort)
+  }
+
+  const handleReset = () => {
+    setUsers(originalUsers.current)
+  }
+
+  const handleDelete = (uuid: string) => {
+    const filteredUsers = users.filter((user) => user.login.uuid !== uuid)
+    setUsers(filteredUsers)
+  }
+
+  const sortedUsers = countrySort
+    ? users.toSorted((a, b) => {
+      return a.location.country.localeCompare(b.location.country)
+    })
+    : users
 
   useEffect(() => {
     fetch('https://randomuser.me/api?results=100')
       .then(res => res.json())
       .then(res => {
         setUsers(res.results)
+        originalUsers.current = res.results
       })
       .catch(err => {
         console.log(err)
@@ -18,9 +47,17 @@ function App () {
   return (
     <div>
       <h1>Lista de usuarios</h1>
-      {
-        JSON.stringify(users)
-      }
+      <header>
+        <button onClick={toggleColors}>Colorear filas</button>
+        <button onClick={toggleSortCountry}>
+          {countrySort ? 'No ordenar por país' : 'Ordenar por país'}
+        </button>
+        <button onClick={handleReset}>Resetear usuarios</button>
+      </header>
+
+      <main>
+        <UsersList users={sortedUsers} showColors={showColors} deleteUser={handleDelete} />
+      </main>
     </div>
   )
 }
